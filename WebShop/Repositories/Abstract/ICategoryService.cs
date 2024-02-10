5d6acccda2bc79efabef4f36971078fc5312af92
iamfilipovich <iamfilipovich@gmail.com>
2024-02-10 20:25:56 +0100

    first_comm

---
+++ WebShop/Repositories/Abstract/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿namespace WebShop.Repositories.Abstract
+{
+    public interface ICategoryService
+    {
+        bool Add (Category model);
+        bool Update (Category model);
+        bool Delete (int id);
+        bool FindById (int id);
+        IEnumerable<Category> GetAll ();
+    }
+}
